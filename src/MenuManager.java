d01b6da6b6378e03e3171ffb32aec01e6a464bb3
Lim Chae sung <dlacotjd59@naver.com>
2021-03-10 18:37:58 +0900

    removed hello.java and added menumanager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,13 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Account Book");
+		System.out.println("2. Delete Account Book");
+		System.out.println("3. Edit Account Book");
+		System.out.println("4. View Account Book");
+		System.out.println("5. Exit");
+
+	}
+
+}
