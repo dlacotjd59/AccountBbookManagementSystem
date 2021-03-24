fef573e01c426a3996cafc5c0b3bd8e278dcb605
Lim Chae sung <dlacotjd59@naver.com>
2021-03-24 23:16:08 +0900

    add menu selection

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -4,9 +4,10 @@ public class MenuManager {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		int num = 0;
+		int num = 5;
 		
 		while (num != 6) {
+		System.out.println("*** Account Management System Menu ***");
 		System.out.println("1. Income");
 		System.out.println("2. Expense");
 		System.out.println("3. Edit Account");
@@ -15,7 +16,47 @@ public class MenuManager {
 		System.out.println("6. Exit");
 		System.out.println("Select one number between 1-6:");
 		num = input.nextInt();
-		
+		if (num == 1) {
+				Income();
 		}
+		else if (num == 2) {
+				Expense();
+		}
+		else if (num == 3) {
+			editAccount();
+		}
+		else if (num == 4) {
+			viewAccount();
+		}
+		else {
+			continue;
+		}
+		}
+	}
+	
+	public static void Income() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("income:");
+		int Income = input.nextInt();	
+		System.out.print("income Name:");
+		String IncomeName = input.next();
+	}
+	public static void Expense() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("expense:");
+		int expense = input.nextInt();
+		System.out.print("expense Name:");
+		String ExpenseName = input.next();
+	}
+		
+	public static void editAccount() {
+		Scanner input = new Scanner(System.in);	
+		System.out.print("income Name or expense Name:");
+		String IncomeName = input.next();
+	}
+	public static void viewAccount() {
+		Scanner input = new Scanner(System.in);	
+		System.out.print("income Name or expense Name:");
+		String IncomeName = input.next();
 	}
 }
